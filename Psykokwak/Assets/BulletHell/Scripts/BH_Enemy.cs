65fde602aade332a0045570297b7526019deb3c0
Weavel35 <clement.chardevel@gmail.com>
2017-06-22 10:48:04 +0200

    Ajout BulletHell v0.1

---
+++ Psykokwak/Assets/BulletHell/Scripts/BH_Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BH_Enemy : MonoBehaviour {
+	
+	public bool changeTrajectory = false;
+	public Vector3 startPos;
+	public Vector3 direction;
+
+	public BH_Player player;
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindObjectOfType<BH_Player>();
+		dir = player.transform.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(changeTrajectory)
+			dir = player.transform.position - this.transform.position;
+		this.transform.position = this.transform.position + (dir.normalized*0.1f);
+		if(this.transform.localPosition.x > 10f || this.transform.localPosition.x < -10f
+		|| this.transform.localPosition.y > 10f || this.transform.localPosition.y < -10f) {
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Psykokwak/Assets/BulletHell/Scripts/BH_Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BH_Spawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public Transform EnemyParent;
+	public float spawnSeconds = 1f; 
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnCoroutine() {
+		GameObject enemy;
+		while (true) {
+			yield return new WaitForSeconds(spawnSeconds);
+			enemy = (GameObject)Instantiate(enemyPrefab);
+			enemy.transform.position = this.transform.position + new Vector3(Random.Range(-5f, 5f), 0f, 0f);
+			enemy.transform.parent = EnemyParent;
+			spawnSeconds*=0.99f;
+		}
+
+	}
+}
