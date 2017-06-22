07033fc97cf09f8149b6a5a83558068d1863d91a
Weavel35 <clement.chardevel@gmail.com>
2017-06-22 17:54:35 +0200

    Fix Spawn

--- Psykokwak/Assets/BulletHell/Scripts/BH_Spawner.cs
+++ Psykokwak/Assets/BulletHell/Scripts/BH_Spawner.cs
@@ -10,7 +10,7 @@ public class BH_Spawner : MonoBehaviour {
 	public float spawnTimeReduction = 0.995f;
 
 	public float enemySpeed = 0.1f;
-
+	public int count = 0;
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(SpawnCoroutine());
@@ -25,13 +25,15 @@ public class BH_Spawner : MonoBehaviour {
 		GameObject enemy;
 		while (true) {
 			yield return new WaitForSeconds(spawnSeconds);
-			if(Random.Range(0, 2) == 1)
+			count++;
+			if (count%25 == 24)
 				enemy = (GameObject)Instantiate(bonusPrefab);
 			else 
 				enemy = (GameObject)Instantiate(enemyPrefab);
 			enemy.transform.position = this.transform.position + new Vector3(Random.Range(-5f, 5f), 0f, 0f);
 			enemy.transform.parent = EnemyParent;
 			enemy.GetComponent<BH_Enemy>().speed = enemySpeed;
+			if (spawnSeconds > 0.01f)
 			spawnSeconds*=0.99f;
 		}
 
