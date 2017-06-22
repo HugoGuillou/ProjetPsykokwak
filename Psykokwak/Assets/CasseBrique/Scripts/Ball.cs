fc9876957c2dc25a458307404a152e204ac020fa
HugoGuillou <6NxC7XPJ>
2017-06-22 11:00:54 +0200

    BreakerBasics

---
+++ Psykokwak/Assets/CasseBrique/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Vector3 _velocity;
+	public float _speed = 1f;
+	public float _maxSpeed = 40f;
+	public float _speedInc = 0.1f;
+	public float _barDeviation = 2f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		_velocity = new Vector3 (1, 1, 0) * _speed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (_velocity * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		float offset = 0f;
+
+		if (coll.gameObject.tag == "Bar") 
+		{
+			Transform bar = coll.gameObject.GetComponent<Bar>().transform;
+
+			float posFromBar = ((transform.position.x - bar.position.x) / bar.localScale.x) * 2;
+			offset = posFromBar * _barDeviation;
+
+		}
+		_speed += _speedInc;
+
+		if (_speed > _maxSpeed) 
+		{
+			_speed = _maxSpeed;
+		}
+			
+		Vector3 dir = Vector3.Reflect(_velocity, coll.contacts[0].normal); 
+
+		dir = new Vector3 (dir.x + offset, dir.y, 0);
+
+		_velocity = dir.normalized * _speed;
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "PitOfDespair") 
+		{
+			//TODO
+			//GameManager.Lose();
+			Debug.Log ("YOU'RE FUCKIND DEAD");
+			
+		}
+	}
+
+}
---
+++ Psykokwak/Assets/CasseBrique/Scripts/Bar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bar : MonoBehaviour {
+
+	private Vector3 _velocity;
+	public float _speed = 0.1f;
+	private bool blockedRight;
+	private bool blockedLeft;
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xAxis = Input.GetAxis ("CB_Horizontal");
+
+		if ((xAxis > 0 && blockedRight) || 
+		    (xAxis < 0 && blockedLeft)) 
+		{
+			return;
+		}
+
+		_velocity = new Vector3 (xAxis, 0, 0) * _speed;
+
+		transform.Translate (_velocity * Time.deltaTime);
+		
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.gameObject.tag == "LBound") {
+			blockedLeft = true;
+		} else if (coll.gameObject.tag == "RBound") {
+			blockedRight = true;
+		}
+	}
+
+	void OnCollisionExit(Collision coll)
+	{
+		if (coll.gameObject.tag == "LBound") {
+			blockedLeft = false;
+		} else if (coll.gameObject.tag == "RBound") {
+			blockedRight = false;
+		}
+	}
+
+}
